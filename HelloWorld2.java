624b99d06fd777122abb91e0046ac45c98721c13
myromr <43197039+myromr@users.noreply.github.com>
2018-12-02 02:07:13 +0800

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,12 @@
+public class HelloWorld2
+{
+   public static void main(String[] args) {
+
+       System.out.println("Name: Romar A. Marcos");
+       System.out.println("Birthday: September 6, 1997");
+       System.out.println("Age: 21 yrs. Old");
+       System.out.println("Sex: Male");
+       System.out.println("Pi: 3.1416");
+    
+   }
+}
